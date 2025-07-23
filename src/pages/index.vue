0bd57bb9f35483c4fc31cc35569269ba14441c9d
muhammadalifh <61005674+muhammadalifh@users.noreply.github.com>
2025-07-23 09:27:29 +0700

    first commit

---
+++ src/pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="services-page">
+        <MainPage
+            :benefits="pageData?.benefits ?? []"
+            :services="pageData?.services ?? []"
+        />
+    </div>
+</template>
+
+<script setup lang="ts">
+import { usePage } from '~/composables/usePage'
+
+const { pageData, getPageData } = usePage()
+
+await getPageData()
+</script>
+
+<style scoped>
+.services-page {
+  overflow-x: hidden;
+}
+</style>
